314cd76d0c07d9612a992b866e95aa4cea137247
martha14045 <119931936+martha14045@users.noreply.github.com>
2022-12-06 21:12:08 +0700

    Add files via upload

---
+++ alpro baru.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int penjumlahan(){
+    int a,b;
+    int hasil;
+
+        printf("Penjumlahan bilangan bulat");
+        printf("Masukkan bilangan pertama : ");
+        scanf("%d", &a);
+        printf("Masukkan bilangan kedua : ");
+        scanf("%d", &b);
+
+        hasil = a+b;
+        printf("hasil dari penjumlahan adalah %d\n", &hasil);
+return 0;
+}
